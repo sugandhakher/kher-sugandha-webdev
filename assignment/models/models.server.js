0ccc7d3da93e623499b23b4c783874d5490dbec0
sugandhakher <sugandhakher@ccs.neu.edu>
2017-03-22 01:44:56 -0400

    test

--- assignment/models/models.server.js
+++ assignment/models/models.server.js
@@ -7,12 +7,12 @@ module.exports = function () {
     // import mongoose library
     var mongoose = require("mongoose");
     var connectionString = 'mongodb://127.0.0.1:27017/spring2017';
-    if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
-        connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-            process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-            process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-            process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-            process.env.OPENSHIFT_APP_NAME;
+    if(process.env.MLAB_USERNAME) {
+        connectionString = process.env.MLAB_USERNAME + ":" +
+            process.env.MLAB_PASSWORD + "@" +
+            process.env.MLAB_HOST + ':' +
+            process.env.MLAB_PORT + '/' +
+            process.env.MLAB_APP_NAME;
     }
     mongoose.connect(connectionString);
 
